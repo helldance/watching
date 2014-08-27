fc2716e37816eed84a1c0180bd73747531927b73
dafeng.xdf <dafeng.xdf@taobao.com>
2014-08-27 17:19:20 +0800

    First commit

---
+++ test/watching.test.js
@@ -0,0 +1,21 @@
+/* ================================================================
+ * watching by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Wed Aug 27 2014 16:53:54 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright 2013 xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+"use strict";
+
+var watching = require('../');
+
+describe('', function(){
+});
+
+
